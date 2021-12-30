031bf705d557f1a671775ca90815c6f735f0623e
Sebastien MICHEL <sebastien.t.michel@renault.com>
2021-12-30 10:02:36 +0100

    feat: add logger to clean_cache
    Signed-off-by: Sebastien MICHEL <sebastien.t.michel@renault.com>

--- git_cdn/cache_handler/clean_cache.py
+++ git_cdn/cache_handler/clean_cache.py
@@ -1,13 +1,35 @@
 # Standard Library
 import argparse
+import logging
 import os
-from datetime import datetime
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version
 from shutil import rmtree
+
+import sentry_sdk
+from structlog import getLogger
 
 from git_cdn.cache_handler.common import find_bundle
 from git_cdn.cache_handler.common import find_git_repo
 from git_cdn.cache_handler.common import find_lfs
 from git_cdn.cache_handler.common import sizeof_fmt
+from git_cdn.log import enable_console_logs
+from git_cdn.log import enable_udp_logs
+
+log = getLogger()
+
+try:
+    GITCDN_VERSION = version("git_cdn")
+except PackageNotFoundError:
+    GITCDN_VERSION = "unknown"
+
+sentry_dsn = os.getenv("SENTRY_DSN")
+if sentry_dsn:
+    sentry_sdk.init(
+        sentry_dsn,
+        release=GITCDN_VERSION,
+        environment=os.getenv("SENTRY_ENV", "dev"),
+    )
 
 
 def mtime(g):
@@ -15,7 +37,12 @@ def mtime(g):
 
 
 def find_older_repo(older_than):
-    git_dirs = [g for g in find_git_repo("git") if g.age > older_than]
+    git_dirs = []
+    for g in find_git_repo("git"):
+        if g.age > older_than:
+            git_dirs.append(g)
+        log.info("git repo", **g.to_dict())
+
     git_dirs.sort(key=mtime)
     return git_dirs
 
@@ -28,9 +55,9 @@ def clean_git_repo(older_than, verbose, delete):
             print(g)
 
     print(f"Number of older git repos: {len(git_dirs)}")
+    total_size = sum([g.size for g in git_dirs])
+    log.info("clean_git_repo stats", clean_size=total_size, clean_files=len(git_dirs))
     if verbose:
-        # calculate the size only on verbose mode
-        total_size = sum([g.size for g in git_dirs])
         print(f"Total size that would be deleted {sizeof_fmt(total_size)}")
 
     if delete:
@@ -39,7 +66,11 @@ def clean_git_repo(older_than, verbose, delete):
 
 
 def clean_lfs(older_than, verbose, delete):
-    lfs_files = [f for f in find_lfs("lfs") if f.age > older_than]
+    lfs_files = []
+    for f in find_lfs("lfs"):
+        if f.age > older_than:
+            lfs_files.append(f)
+        log.info("lfs file", **f.to_dict())
 
     lfs_files.sort(key=mtime)
 
@@ -50,6 +81,7 @@ def clean_lfs(older_than, verbose, delete):
     total_size = sum([f.size for f in lfs_files])
     print(f"Number of lfs files to be cleaned: {len(lfs_files)}")
     print(f"Total size that would be deleted {sizeof_fmt(total_size)}")
+    log.info("clean_lfs stats", clean_size=total_size, clean_files=len(lfs_files))
 
     if delete:
         for f in lfs_files:
@@ -57,7 +89,12 @@ def clean_lfs(older_than, verbose, delete):
 
 
 def clean_bundle(older_than, verbose, delete):
-    bundles = [f for f in find_bundle() if f.age > older_than]
+    bundles = []
+    for b in find_bundle("bundles"):
+        if b.age > older_than:
+            bundles.append(b)
+        log.info("bundle file", **b.to_dict())
+
     bundles.sort(key=mtime)
 
     if verbose:
@@ -67,10 +104,23 @@ def clean_bundle(older_than, verbose, delete):
     total_size = sum([f.size for f in bundles])
     print(f"Number of bundle files to be cleaned: {len(bundles)}")
     print(f"Total size that would be deleted {sizeof_fmt(total_size)}")
+    log.info("clean_bundles stats", clean_size=total_size, clean_files=len(bundles))
 
     if delete:
         for f in bundles:
             f.delete()
+
+
+def setup_logging():
+    logging.basicConfig(level=logging.DEBUG)
+    log_server = os.getenv("LOGGING_SERVER")
+    if log_server is not None:
+        print("LOGGING TO VECTOR")
+        host, port = log_server.split(":")
+        enable_udp_logs(host, int(port), GITCDN_VERSION)
+    else:
+        print("LOGGING TO CONSOLE")
+        enable_console_logs()
 
 
 def clean_cdn_cache():
@@ -120,5 +170,10 @@ def clean_cdn_cache():
         clean_bundle(args.lfs_older_than, args.verbose, args.delete)
 
 
-if __name__ == "__main__":
+def main():
+    setup_logging()
     clean_cdn_cache()
+
+
+if __name__ == "__main__":
+    main()
